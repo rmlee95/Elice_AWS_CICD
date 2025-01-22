fb10806f7d876ecfe23528ab31d23f359b0a92c7
rmlee95 <bybark4785@gmail.com>
2025-01-22 17:02:16 +0900

    Update TestController.java

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "AWS CICD TEST";
+    return "Hello, World!";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
